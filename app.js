f687df26066b192f0deca109fd4774e3211aba63
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-19 18:44:43 +0300

    Did fetch cookies and linked with frontend

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const games = require("./games");
+const cors = require("cors");
+
+app.get("/", (req, res) => {
+  console.log("HELLO");
+  res.json({ message: "Hello World" });
+});
+
+app.get("/games", (req, res) => {
+  res.json(games);
+});
+
+app.use(cors());
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
+});
