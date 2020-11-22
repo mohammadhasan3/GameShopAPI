bbb9368b46347f96f0fadfbbdf0911c38173d32b
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-22 16:59:15 +0300

    Added delete method

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const games = require("./games");
+let games = require("./games");
 const cors = require("cors");
 app.use(cors());
+
 app.get("/", (req, res) => {
   console.log("HELLO");
   res.json({ message: "Hello World" });
@@ -10,6 +11,17 @@ app.get("/", (req, res) => {
 
 app.get("/games", (req, res) => {
   res.json(games);
+});
+
+app.delete("/games/:gameId", (req, res) => {
+  const { gameId } = req.params;
+  const foundGame = games.find((game) => game.id === +gameId);
+  if (foundGame) {
+    games = games.filter((game) => game.id !== +gameId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Game not found" });
+  }
 });
 
 app.listen(8000, () => {
