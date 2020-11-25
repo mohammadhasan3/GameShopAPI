6caf8590d320c369f48bf078ebd30c96b48086d7
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-25 17:05:59 +0300

    Set up database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db/models");
 const bodyParser = require("body-parser");
 const gameRoutes = require("./routes/games");
 const app = express();
@@ -8,6 +9,16 @@ const cors = require("cors");
 app.use(cors());
 app.use("/games", gameRoutes);
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+const run = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
